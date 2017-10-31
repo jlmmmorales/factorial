b1e30fd062b6d2430491a0ee31cf46c416c1840d
jose78 <jlmmmorales@gmail.com>
2017-10-31 19:52:16 +0100

    Contemplo los negativos.

--- src/main/java/orgCursoAccenture/factorial/Factorial.java
+++ src/main/java/orgCursoAccenture/factorial/Factorial.java
@@ -1,14 +1,17 @@
 package orgCursoAccenture.factorial;
 
+/**
+ * Class that computes the factorial of an integer (long) number
+ * @author José Luis Martin Morales
+ *
+ */
 public class Factorial {
 
 	public long computar(long valor) {
-		if ((valor == 0) || (valor == 1)) {
+		if (valor < 0) {
+			throw new RuntimeException("ERROR: The value is negative." + valor);
+		} else if ((valor == 0) || (valor == 1)) {
 			return 1;
-		} else if (valor == 2) {
-			return 2;
-		} else if (valor == 3) {
-			return 6;
 		} else {
 			return valor * computar(valor - 1);
 		}

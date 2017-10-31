0a2cc3b673069968e05e0d0c07c340339fb0993a
jose78 <jlmmmorales@gmail.com>
2017-10-31 20:05:51 +0100

    Refactorizacion Factorial

--- src/main/java/orgCursoAccenture/factorial/Factorial.java
+++ src/main/java/orgCursoAccenture/factorial/Factorial.java
@@ -2,18 +2,26 @@ package orgCursoAccenture.factorial;
 
 /**
  * Class that computes the factorial of an integer (long) number
+ * 
  * @author José Luis Martin Morales
  *
  */
 public class Factorial {
 
 	public long computar(long valor) {
+		long result;
 		if (valor < 0) {
 			throw new RuntimeException("ERROR: The value is negative." + valor);
 		} else if ((valor == 0) || (valor == 1)) {
-			return 1;
+			result = 1;
 		} else {
-			return valor * computar(valor - 1);
+			result = valor * computar(valor - 1);
 		}
+		
+		return result;
+	}
+
+	public String toString() {
+		return "";
 	}
 }

e9c7e1c3216003178fcf8d889ba93269d74d4f85
jose78 <jlmmmorales@gmail.com>
2017-11-02 17:44:17 +0100

    Empezamos el jueves

--- src/test/java/orgCursoAccenture/factorial/FactorialTest.java
+++ src/test/java/orgCursoAccenture/factorial/FactorialTest.java
@@ -1,76 +1,75 @@
 package orgCursoAccenture.factorial;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Before;
 import org.junit.Test;
 
 public class FactorialTest {
 
-	private Factorial factorial;
+  private Factorial factorial;
 
-	@Before
-	public void init() {
-		factorial = new Factorial();
-	}
+  @Before
+  public void init() {
+    factorial = new Factorial();
+  }
 
-	@Test
-	public void testFactorialOfZeroReturnOne() {
-		// Factorial factorial = new Factorial();
+  @Test
+  public void testFactorialOfZeroReturnOne() {
+    // Factorial factorial = new Factorial();
 
-		long result = factorial.computar(0);
-		long expectedValue = 1;
+    long result = factorial.computar(0);
+    long expectedValue = 1;
 
-		assertEquals(expectedValue, result);
-	}
+    assertEquals(expectedValue, result);
+  }
 
-	@Test
-	public void testFactorialOfOneReturnTwo() {
-		// Factorial factorial = new Factorial();
+  @Test
+  public void testFactorialOfOneReturnTwo() {
+    // Factorial factorial = new Factorial();
 
-		long result = factorial.computar(1);
-		long expectedValue = 1;
+    long result = factorial.computar(1);
+    long expectedValue = 1;
 
-		assertEquals(expectedValue, result);
-	}
+    assertEquals(expectedValue, result);
+  }
 
-	@Test
-	public void testFactorialOfTwoReturnTwo() {
-		// Factorial factorial = new Factorial();
+  @Test
+  public void testFactorialOfTwoReturnTwo() {
+    // Factorial factorial = new Factorial();
 
-		long result = factorial.computar(2);
-		long expectedValue = 2;
+    long result = factorial.computar(2);
+    long expectedValue = 2;
 
-		assertEquals(expectedValue, result);
-	}
+    assertEquals(expectedValue, result);
+  }
 
-	@Test
-	public void testFactorialOfThreeReturnSix() {
-		// Factorial factorial = new Factorial();
+  @Test
+  public void testFactorialOfThreeReturnSix() {
+    // Factorial factorial = new Factorial();
 
-		long result = factorial.computar(3);
-		long expectedValue = 6;
+    long result = factorial.computar(3);
+    long expectedValue = 6;
 
-		assertEquals(expectedValue, result);
-	}
+    assertEquals(expectedValue, result);
+  }
 
-	@Test
-	public void testFactorialOfFiveReturnOneHundredAndTwenty() {
-		// Factorial factorial = new Factorial();
+  @Test
+  public void testFactorialOfFiveReturnOneHundredAndTwenty() {
+    // Factorial factorial = new Factorial();
 
-		long result = factorial.computar(5);
-		long expectedValue = 120;
+    long result = factorial.computar(5);
+    long expectedValue = 120;
 
-		assertEquals(expectedValue, result);
-	}
-	
-	@Test (expected = RuntimeException.class)
-	public void testFactorialOfNegativeValueReturnException() {
-		// Factorial factorial = new Factorial();
+    assertEquals(expectedValue, result);
+  }
 
-		long result = factorial.computar(-1);
-		long expectedValue = 0;
+  @Test(expected = RuntimeException.class)
+  public void testFactorialOfNegativeValueReturnException() {
+    // Factorial factorial = new Factorial();
 
-		assertEquals(expectedValue, result);
-	}
+    long result = factorial.computar(-1);
+    long expectedValue = 0;
+
+    assertEquals(expectedValue, result);
+  }
 }
